7f53bf5803d8be300bca17ca7cfcfde5d21717c8
Iristech Systems <contact@iristech.systems>
2025-07-13 08:00:34 -0400

    docs: add comprehensive Sphinx documentation
    - Complete Sphinx setup with ansys_sphinx_theme
    - Professional documentation structure with tutorials
    - API reference documentation framework
    - Installation, quickstart, and tutorial guides
    - Contributing guidelines and changelog
    - Custom CSS with quantum-themed styling
    - SVG logo integration for documentation
    - Build system with Makefile and requirements

---
+++ docs/conf.py
@@ -0,0 +1,197 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'QuantumEngine'
+copyright = '2025, Iristech Systems'
+author = 'Iristech Systems'
+release = '0.1.2'
+version = '0.1.2'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.duration',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx_copybutton',
+    'myst_parser',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The suffix(es) of source filenames.
+source_suffix = {
+    '.rst': None,
+    '.md': 'myst_parser',
+}
+
+# The master toctree document.
+master_doc = 'index'
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.
+html_theme = 'ansys_sphinx_theme'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.
+html_theme_options = {
+    "github_url": "https://github.com/iristech-systems/QuantumEngine",
+    "show_prev_next": False,
+    "show_breadcrumbs": True,
+    "additional_breadcrumbs": [
+        ("PyPI", "https://pypi.org/project/quantumengine/"),
+    ],
+    "icon_links": [
+        {
+            "name": "Support",
+            "url": "https://github.com/iristech-systems/QuantumEngine/discussions",
+            "icon": "fa fa-comment fa-fw",
+        },
+    ],
+    "use_edit_page_button": False,  # Disabled to fix the build error
+    "navigation_with_keys": True,
+    "collapse_navigation": True,
+}
+
+# Context for edit page button (if enabled later)
+html_context = {
+    "github_user": "iristech-systems",
+    "github_repo": "QuantumEngine", 
+    "github_version": "main",
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Custom sidebar templates, must be a dictionary that maps document names
+# to template names.
+html_sidebars = {
+    "**": ["sidebar-nav-bs"],
+}
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# Set the permalink icon
+html_permalinks_icon = "<span>¶</span>"
+
+# -- Extension configuration -------------------------------------------------
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
+# Autodoc settings
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+autodoc_typehints = 'description'
+autodoc_typehints_description_target = 'documented'
+
+# Autosummary settings
+autosummary_generate = True
+autosummary_imported_members = True
+
+# Intersphinx mapping
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+}
+
+# Todo extension settings
+todo_include_todos = True
+
+# Copy button settings
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
+
+# MyST parser settings
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "substitution",
+    "tasklist",
+]
+
+# HTML output options
+html_title = f"{project} v{version}"
+html_short_title = project
+html_show_sourcelink = True
+html_show_sphinx = True
+html_show_copyright = True
+
+# LaTeX output options
+latex_elements = {
+    'papersize': 'letterpaper',
+    'pointsize': '10pt',
+    'preamble': '',
+    'fncychap': '',
+    'printindex': '',
+}
+
+# Grouping the document tree into LaTeX files
+latex_documents = [
+    (master_doc, 'QuantumEngine.tex', 'QuantumEngine Documentation',
+     'Iristech Systems', 'manual'),
+]
+
+# -- Custom configuration ---------------------------------------------------
+
+# Add custom CSS
+def setup(app):
+    """Set up the Sphinx application."""
+    app.add_css_file('custom.css')
+
+# Suppress warnings for external links
+suppress_warnings = ['image.nonlocal_uri']
+
+# Set up source file encoding
+source_encoding = 'utf-8-sig'
